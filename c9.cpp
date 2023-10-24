2cb695ed09d23351c943907c96bf7dab34fc888f
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-10-24 22:07:59 +0500

    Add files via upload

---
+++ c9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	char charac;
+	cout<<"enter the number"<<endl;
+	cin>>charac;
+	int num1=charac;
+	cout<<"The Ascii code for"<<charac<<"is"<<num1<<endl;
+	return 0;
+}
